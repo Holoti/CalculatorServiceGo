cc357aa6c4c562bb8707de1237aa2a89b2add25d
Holoti <106698219+Holoti@users.noreply.github.com>
2024-12-28 12:18:14 +0500

    refactor: move handlers, middlewares to separate packages

--- internal/application/application.go
+++ internal/application/application.go
@@ -1,13 +1,11 @@
 package application
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"math"
 	"net/http"
 
-	"github.com/holoti/CalculatorServiceGo/pkg/calculate"
+	"github.com/holoti/CalculatorServiceGo/internal/handlers"
+	"github.com/holoti/CalculatorServiceGo/internal/middleware"
 )
 
 type Config struct {
@@ -24,61 +22,7 @@ func New(config Config) *Application {
 	}
 }
 
-func CalcHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var req struct {
-		Expression string `json:"expression"`
-	}
-
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		log.Printf("error reading request: %v)", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(map[string]string{
-			"error": "Internal server error",
-		})
-		return
-	}
-
-	if req.Expression == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(map[string]string{
-			"error": "Bad request body",
-		})
-		return
-	}
-
-	result, err := calculate.Calc(req.Expression)
-	if err != nil {
-		w.WriteHeader(http.StatusUnprocessableEntity)
-		json.NewEncoder(w).Encode(map[string]string{
-			"error": fmt.Sprintf("Expression is not valid: %v", err),
-		})
-		return
-	}
-
-	rounded := math.Round(result*math.Pow10(6)) / math.Pow10(6)
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{
-		"result": fmt.Sprint(rounded),
-	})
-}
-
-func ValidateMethod(next http.HandlerFunc) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			w.Header().Set("Content-type", "application/json")
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			json.NewEncoder(w).Encode(map[string]string{
-				"error": "Method not allowed; use POST",
-			})
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
 func (a *Application) Run() {
-	// http.HandleFunc("/api/v1/calculate", CalcHandler)
-	http.Handle("/api/v1/calculate", ValidateMethod(CalcHandler))
+	http.Handle("/api/v1/calculate", middleware.ValidateMethod(handlers.CalcHandler))
 	http.ListenAndServe(fmt.Sprintf("localhost:%d", a.Cfg.Port), nil)
 }
---
+++ internal/handlers/calc_handler.go
@@ -0,0 +1,50 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math"
+	"net/http"
+
+	"github.com/holoti/CalculatorServiceGo/pkg/calculate"
+)
+
+func CalcHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var req struct {
+		Expression string `json:"expression"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		log.Printf("error reading request: %v)", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "Internal server error",
+		})
+		return
+	}
+
+	if req.Expression == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "Bad request body",
+		})
+		return
+	}
+
+	result, err := calculate.Calc(req.Expression)
+	if err != nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": fmt.Sprintf("Expression is not valid: %v", err),
+		})
+		return
+	}
+
+	rounded := math.Round(result*math.Pow10(6)) / math.Pow10(6)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{
+		"result": fmt.Sprint(rounded),
+	})
+}
